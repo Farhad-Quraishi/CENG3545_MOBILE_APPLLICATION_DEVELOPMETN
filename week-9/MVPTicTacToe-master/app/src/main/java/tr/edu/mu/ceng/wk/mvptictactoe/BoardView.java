62c8b6277762f4c2b47ef81947ee5019a10cad97
Farhad-Quraishi <farhadquraishi77@gmail.com>
2022-11-19 15:01:02 +0300

    9th commit

---
+++ week-9/MVPTicTacToe-master/app/src/main/java/tr/edu/mu/ceng/wk/mvptictactoe/BoardView.java
@@ -0,0 +1,17 @@
+package tr.edu.mu.ceng.wk.mvptictactoe;
+
+public interface BoardView {
+
+    char PLAYER_1_SYMBOL = 'X';
+    char PLAYER_2_SYMBOL = 'O';
+
+    byte DRAW = 0;
+    byte PLAYER_1_WINNER = 1;
+    byte PLAYER_2_WINNER = 2;
+
+    void newGame();
+
+    void putSymbol(char symbol, byte row, byte col);
+
+    void gameEnded(byte winner);
+}
